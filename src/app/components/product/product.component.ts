f246a6768dcb3260860af04e3d14aeb4362790d9
MateusL21 <mateusleonel27@gmail.com>
2024-10-10 20:29:26 -0300

    finalizando o módulo 6

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Category } from 'src/app/interfaces/Category';
 import { Product } from 'src/app/interfaces/Product';
 
@@ -24,10 +24,10 @@ export class ProductComponent implements OnInit, OnChanges {
   constructor(private FormBuilder: FormBuilder) { 
     this.formGroupProduct = this.FormBuilder.group({
       id : {value:null, disabled:true},
-      name : [''],
-      description : [''],
-      category : [''],
-      price : [''],
+      name : ['', [Validators.required, Validators.minLength(3)]],
+      description : ['', [Validators.required]],
+      category : ['', [Validators.required]],
+      price : ['', [Validators.required]],
       newProduct : [''],
       promotion : ['']
     })
@@ -45,8 +45,10 @@ export class ProductComponent implements OnInit, OnChanges {
   }
 
   save(){
-    Object.assign(this.product, this.formGroupProduct.value);
+    if(this.formGroupProduct.valid){
+      Object.assign(this.product, this.formGroupProduct.value);
     this.saveEmitter.emit(true);
+    }
   }
 
   cancel(){
@@ -56,4 +58,11 @@ export class ProductComponent implements OnInit, OnChanges {
   selectedCategory(category1: Category, category2 : Category){
     return category1 && category2 ? category1.id === category2.id : false;
   }
+  get pfgName() {return this.formGroupProduct.get("name")}  
+
+  get pfgDescription() {return this.formGroupProduct.get("description")}
+
+  get pfgPrice() {return this.formGroupProduct.get("price")}
+
+  get pfgCategory() {return this.formGroupProduct.get("category")}
 }
